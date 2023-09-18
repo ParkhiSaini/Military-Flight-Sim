520873c95784b92179cb554a5ff7ba8865eb5819
Akash Sinha <66449020+theskysinha@users.noreply.github.com>
2023-09-18 11:20:53 +0530

    merge conflicts

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MainMenu : MonoBehaviour
+{
+    
+    public AudioSource buttonClick;
+    public Button droneBtn;
+    public Button flightBtn;
+    public Button droneExitBtn;
+    public Button flightExitBtn;
+    public GameObject droneMenu;
+    public GameObject flightMenu;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+    }
+
+    public void PlayButton(){
+        buttonClick.Play();
+    }
+
+    public void selectedMenu(){
+        if(EventSystem.current.currentSelectedGameObject == droneBtn.gameObject){
+            droneMenu.SetActive(true);
+            flightMenu.SetActive(false);
+        }
+        else if(EventSystem.current.currentSelectedGameObject == flightBtn.gameObject){
+            flightMenu.SetActive(true);
+            droneMenu.SetActive(false);
+        }
+    }
+
+    public void exitMenu(){
+        Application.Quit();
+    }
+
+    public void StartDrone(){
+        UnityEngine.SceneManagement.SceneManager.LoadScene("TrainingGround");
+    }
+
+}
