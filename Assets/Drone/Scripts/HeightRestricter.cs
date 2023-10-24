c7484de875ac3b18fed44ce8725a84818913e2bf
Akash Sinha <66449020+theskysinha@users.noreply.github.com>
2023-10-24 22:51:49 +0530

    added domain

--- Assets/Drone/Scripts/HeightRestricter.cs
+++ Assets/Drone/Scripts/HeightRestricter.cs
@@ -7,28 +7,48 @@ public class HeightRestricter : MonoBehaviour
     public Transform dronePos;
     public GameObject fade;
     public GameObject finalWarning;
-
+    public double positiveXLimit = 0.0;
+    public double negativeXLimit = 0.0;
+    public double positiveZLimit = 0.0;
+    public double negativeZLimit = 0.0;
     private void HeightLimit(){
         if(dronePos != null){
-            if(dronePos.position.y < 65){
+            if(dronePos.position.y < 70){
                 fade.SetActive(false);
             }
-            if(dronePos.position.y > 65 && dronePos.position.y < 67){
+            if(dronePos.position.y > 70 && dronePos.position.y < 73){
             fade.SetActive(true);
             }
-            else if(dronePos.position.y > 67 && dronePos.position.y < 69){
+            else if(dronePos.position.y > 73 && dronePos.position.y < 75){
                 fade.SetActive(false);
             }
-            else if (dronePos.position.y > 71 && dronePos.position.y < 74){
+            else if (dronePos.position.y > 78 && dronePos.position.y < 80){
                 finalWarning.SetActive(false);
             }
-            else if (dronePos.position.y > 69 && dronePos.position.y < 71){
+            else if (dronePos.position.y > 75 && dronePos.position.y < 77){
                 fade.SetActive(true);
             }
-            if (dronePos.position.y > 74){
+            if (dronePos.position.y > 82){
                 finalWarning.SetActive(true);
             } else{
                 finalWarning.SetActive(false);
+            }
+        }
+    }
+
+    public void mapRestrictor(){
+        if(dronePos != null){
+            if(dronePos.position.x > positiveXLimit){
+                dronePos.position = new Vector3((float)positiveXLimit, dronePos.position.y, dronePos.position.z);
+            }
+            if(dronePos.position.x < negativeXLimit){
+                dronePos.position = new Vector3((float)negativeXLimit, dronePos.position.y, dronePos.position.z);
+            }
+            if(dronePos.position.z > positiveZLimit){
+                dronePos.position = new Vector3(dronePos.position.x, dronePos.position.y, (float)positiveZLimit);
+            }
+            if(dronePos.position.z < negativeZLimit){
+                dronePos.position = new Vector3(dronePos.position.x, dronePos.position.y, (float)negativeZLimit);
             }
         }
     }
