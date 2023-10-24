1255cc98d29adaceb802ea7fdb5204335535d300
Parkhi Saini <90965313+ParkhiSaini@users.noreply.github.com>
2023-10-24 22:58:04 +0530

    photon

---
+++ Assets/Scripts/Photon/PlayerDetails.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+using UnityEngine.UI;
+
+
+public class PlayerDetails : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField nameInputField = null;
+    [SerializeField] private Button continueButton =null;
+
+    private const string PlayerPrefsNameKey = "PlayerName";
+    
+    private void Start() => SetUpInputField();
+
+    private void SetUpInputField()
+    {
+        
+        if (!PlayerPrefs.HasKey(PlayerPrefsNameKey)) {return;}
+
+        string defaultName = PlayerPrefs.GetString(PlayerPrefsNameKey);
+
+        nameInputField.text = defaultName;
+
+        SetPlayerName(defaultName);
+    }
+
+    public void SetPlayerName(string name)
+    {
+        continueButton.interactable  = !string.IsNullOrEmpty(name);
+    }
+
+    public void SavePlayerName()
+    {
+        string playerName =nameInputField.text;
+        PhotonNetwork.NickName =playerName;
+        PlayerPrefs.SetString(PlayerPrefsNameKey, playerName);
+
+    }
+
+}
