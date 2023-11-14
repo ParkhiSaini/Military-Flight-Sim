14f0cd82bfe27fbff8ae486c4e351c40a1a00558
Parkhi Saini <90965313+ParkhiSaini@users.noreply.github.com>
2023-11-14 13:34:34 +0530

    Options Menu Done

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -2,11 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
-
+using UnityEngine.UI;
+using TMPro;
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
     public AudioMixer SFX;
+    public TMP_Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        
+        int currentResolutionIndex = 0;
+        for(int i =0; i< resolutions.Length; i++)
+        {
+            string option =resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+
+    }
+
+    public void SetResolution (int resolutionIndex)
+    {
+        Resolution resolution  = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("Volume", volume);
@@ -14,5 +48,13 @@ public class SettingsMenu : MonoBehaviour
     public void SetSFXVolume(float sfxvolume)
     {
         SFX.SetFloat("SFX", sfxvolume);
+    }
+    public void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
     }
 }
